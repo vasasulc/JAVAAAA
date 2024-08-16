f69edba2bd6eb7457e50492cbca61838133c5f23
Václav Šulc <sulcvac@student.uzlabina.cz>
2024-08-16 16:38:27 +0200

    přidáno přidávání úkolů a jejich splnění

--- Main.java
+++ Main.java
@@ -1,12 +1,60 @@
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+
+        // vytvoření listu s úkolama
         ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("x", "y", false));
-        tasks.add(new Task("z", "n", false));
-        tasks.add(new Task("www", "yxyxyxy", true));
+
+        // přidání úkolů
+        tasks.add(new Task("Uklidit pokoj", false));
+        tasks.add(new Task("Nakoupit jídlo", false));
+        tasks.add(new Task("dodělat program", true));
+
+        // vytvoření scanneru
+        Scanner scanner = new Scanner(System.in);
+
+        // výběr akce
+        System.out.println("Pro přidání úkolu napište: \"Přidat\"");
+        System.out.println("Pro přidání úkolu napište: \"Splnit\"");
+
+        // sken inputu
+        String Input = scanner.nextLine();
+
+        // provedení akce, kterou si uživatel vybral
+        if (Input.equals("Přidat")) {
+
+            System.out.println("Název úkolu:");
+            String nazevUkolu = scanner.nextLine();
+            tasks.add(new Task(nazevUkolu, false));
+
+        } else if (Input.equals("Splnit")) {
+
+            System.out.print("Jaký úkol si splnil?");
+            Task.vypisUkolu(tasks);
+            String nazevUkolu = scanner.nextLine();
+            Boolean ukolNalezen = false;
+
+            for (Task task : tasks) {
+                if (nazevUkolu.equals(task.nazevUkolu) && task.splneno.equals(false)) {
+                    task.splneno = true;
+                    System.out.println("Úkol " + nazevUkolu + " byl označen jako splněný");
+                    ukolNalezen = true;
+                    break;
+                } else if (nazevUkolu.equals(task.nazevUkolu) && task.splneno.equals(true)) {
+                    System.out.println("Úkol " + nazevUkolu + " už je splněn");
+                    ukolNalezen = true;
+                    break;
+                }
+            }
+
+            if (!ukolNalezen) {
+                System.out.println("Úkol není na seznamu");
+            }
+ 
+        }
+        // výpis všech úkolů
         Task.vypisUkolu(tasks);
     }
 }
--- Task.java
+++ Task.java
@@ -2,20 +2,23 @@ import java.util.ArrayList;
 
 public class Task {
     String nazevUkolu;
-    String popisUkolu;
     Boolean splneno;
 
-    Task(String nazevUkolu, String popisUkolu, Boolean splneno){
+    Task(String nazevUkolu,Boolean splneno){
         this.nazevUkolu = nazevUkolu;
-        this.popisUkolu = popisUkolu;
         this.splneno = splneno;
     }
 
     static void vypisUkolu(ArrayList<Task>tasks){
+
+        System.out.println();
+        System.out.println("Úkoly:");
+        System.out.println();
+
         for (Task task : tasks){
-            System.out.println(task.nazevUkolu);
-            System.out.println(task.popisUkolu);
-            System.out.println(task.splneno);
+            System.out.println("Úkol: "+task.nazevUkolu);
+            System.out.print("Splněn: ");
+            System.out.println(task.splneno.equals(true) ? "Ano" : "Ne");
             System.out.println();
         }
     }
