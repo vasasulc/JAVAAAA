e41b099fdbcac6171e066af57196078937fbd27d
vasasulc <vasa.sulc@gmail.com>
2024-08-20 15:09:57 +0200

    pridan generator obrazku

--- Honza/Main.java
+++ Honza/Main.java
@@ -7,22 +7,36 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args){
-        ImageIcon image = new ImageIcon("Honza\\honza.png");
+        ImageIcon image1 = new ImageIcon("Honza\\honza.png");
+        ImageIcon image2 = new ImageIcon("Honza\\honza2.jpg");
+        ImageIcon image3 = new ImageIcon("Honza\\honza3.jpg");
+        ImageIcon image4 = new ImageIcon("Honza\\honza4.jpg");
         JLabel label = new JLabel();
-        label.setIcon(image);
         Random random = new Random();
 
+
         for (int j = 0; j < Integer.MAX_VALUE; j += 1) {
+            int cislo = random.nextInt(4)+1;
             JFrame frame = new JFrame();
+            switch (cislo){
+                case 1: label.setIcon(image1);
+                    break;
+                case 2: label.setIcon(image2);
+                    break;
+                case 3: label.setIcon(image3);
+                    break;
+                case 4: label.setIcon(image4);
+                    break;
+            }
             frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
             frame.add(label);
-            int x = random.nextInt(1558);
-            int y = random.nextInt(590);
+            int x = random.nextInt(1420);
+            int y = random.nextInt(580);
             frame.setLocation(x, y);
             frame.setVisible(true);
-            frame.pack();
+            frame.setSize(500, 500);
             try {
-                Thread.sleep(500);
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
