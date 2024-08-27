6acb7fe315f58490f0bfe63a838731752341aa99
vasasulc <vasa.sulc@gmail.com>
2024-08-27 14:32:37 +0200

    added exception for division by zero

--- kalkulacka/calculator.java
+++ kalkulacka/calculator.java
@@ -144,15 +144,24 @@ public class calculator implements ActionListener {
             switch (operator){
 
                 case '+': result = num1 + num2;
+                    textField.setText(String.valueOf(result));
                 break;
                 case '-': result = num1 - num2;
+                    textField.setText(String.valueOf(result));
                 break;
                 case '*': result = num1 * num2;
+                    textField.setText(String.valueOf(result));
                 break;
-                case '/': result = num1 / num2;
+                case '/':
+                    if (num2 == 0.0)
+                    {
+                        textField.setText("Cant divide by zero");
+                        break;
+                    }
+                    result = num1 / num2;
+                    textField.setText(String.valueOf(result));
                 break;
             }
-            textField.setText(String.valueOf(result));
             num1 = result;
         }
 
